0ad22974c7110c1ada6dae795b30585252847a3d
Deepanshu <degupta@deqode.com>
2026-09-23 23:55:36 +0530

    Day1 commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def home():
+    return {"message":"Welcome"}
+print(app)
+
+# path parameter: resource identity identify a specific item , in url itself, no optional always mandatory or else will fail the url
+@app.get("/item/{item_id}")
+def get_item(item_id:int):
+    return {"item_id":item_id}
+
+#  query param: after ? in key:value pair , optional allowed, used for searchin , filtering like in ecomerce for product filtering
+@app.get("/search")
+def search(q:str):
+    return{"q":q}
+
+@app.get("/item_search/{item_id}")
+def search_item(item_id:int,q:str=""):
+    return {
+        "item_id":item_id,
+        "q":q
+    }
